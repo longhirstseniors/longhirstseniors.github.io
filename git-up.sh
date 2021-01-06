f42bf6ac0e042bdce7817c0896f2b15f529ee823
longhirstseniors <lhgseniors@gmail.com>
2021-01-06 10:08:49 +0000

    committed local changes

---
+++ git-up.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# longhirstseniors
+# fc0a7eed06be5dc590c06ab984ea7e96e81772a6
+# c46170bc1d7522f4101310b4aff9b7c6f3fb1fab
+cd ~/longhirstseniors.github.io
+git commit -am "committed local changes"
+sleep 2
+
+expect ../git.exp
+
+
+
